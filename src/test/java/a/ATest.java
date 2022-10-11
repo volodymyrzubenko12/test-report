b9a445f61ecf2dae664363b61932327cb2ee5145
zubenko <zubenko>
2022-10-11 19:20:10 +0300

    Initial commit

---
+++ src/test/java/a/ATest.java
@@ -0,0 +1,14 @@
+package a;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ATest {
+
+    @Test
+    void test() {
+        assertEquals(1, 2);
+    }
+
+}

40e1b39bfced0ee5455cdcf4bd8b0e447496730b
zubenko <zubenko>
2022-10-13 14:01:23 +0300

    h

--- src/main/java/a/A.java
+++ src/main/java/a/A.java
@@ -3,6 +3,7 @@ package a;
 public class A {
     public static void main(String[] args) {
         String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
+        String s2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS22222222";
 
     }
 }

eb848483d3db27fec6aba45217f73f07427ea3bd
zubencko <email@>
2023-02-05 13:09:55 +0200

    aa

--- src/main/java/a/C.java
+++ src/main/java/a/C.java
@@ -8,31 +8,4 @@ public class C {
             System.out.println();
         }
     }
-
-    public void m7(){
-        System.out.println("");
-        if (true){
-            System.out.println();
-        }
-    }
-
-
-    public void m85(){
-        System.out.println("");
-        if (true){
-            System.out.println();
-        }
-    }
-
-    public void m8(){
-        System.out.println("");
-    }
-
-    public void m6(){
-        System.out.println("");
-    }
-
-    public void m4(){
-        System.out.println("");
-    }
 }
---
+++ src/test/java/a/CTest.java
@@ -0,0 +1,14 @@
+package a;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CTest {
+
+    @Test
+    void m(){
+        new C().m();
+    }
+
+}

cc653c49ce92d681cdb307b8887e2b4d03fe024b
zubencko <email@>
2023-02-05 13:04:41 +0200

    aa

--- src/main/java/a/C.java
+++ src/main/java/a/C.java
@@ -4,15 +4,24 @@ public class C {
 
     public void m(){
         System.out.println("");
+        if (true){
+            System.out.println();
+        }
     }
 
     public void m7(){
         System.out.println("");
+        if (true){
+            System.out.println();
+        }
     }
 
 
     public void m85(){
         System.out.println("");
+        if (true){
+            System.out.println();
+        }
     }
 
     public void m8(){
